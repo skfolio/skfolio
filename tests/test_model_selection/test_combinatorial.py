ae83967580c2ba1b1e25ba767659a75c5a97971e
Hugo Delatte <delatte.hugo@gmail.com>
2024-05-20 00:33:02 +0300

    feat(combinatorial): optimal_folds_number function for CombinatorialPurgedCV (#53)

--- tests/test_model_selection/test_combinatorial.py
+++ tests/test_model_selection/test_combinatorial.py
@@ -1,5 +1,10 @@
+import math
+
 import numpy as np
-from skfolio.model_selection import CombinatorialPurgedCV
+import pytest
+
+from skfolio.model_selection import CombinatorialPurgedCV, optimal_folds_number
+from skfolio.model_selection._combinatorial import _avg_train_size, _n_test_paths
 
 
 def assert_split_equal(split, res):
@@ -118,28 +123,32 @@ def test_combinatorial_purged_cv():
     assert cv.n_test_paths == 4
     assert np.array_equal(
         cv.test_set_index,
-        np.array([
-            [0, 1],
-            [0, 2],
-            [0, 3],
-            [0, 4],
-            [1, 2],
-            [1, 3],
-            [1, 4],
-            [2, 3],
-            [2, 4],
-            [3, 4],
-        ]),
+        np.array(
+            [
+                [0, 1],
+                [0, 2],
+                [0, 3],
+                [0, 4],
+                [1, 2],
+                [1, 3],
+                [1, 4],
+                [2, 3],
+                [2, 4],
+                [3, 4],
+            ]
+        ),
     )
     assert np.array_equal(
         cv.binary_train_test_sets,
-        np.array([
-            [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0],
-            [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0],
-        ]),
+        np.array(
+            [
+                [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0],
+            ]
+        ),
     )
     assert np.array_equal(
         cv.recombined_paths,
@@ -149,16 +158,100 @@ def test_combinatorial_purged_cv():
     )
     assert np.array_equal(
         cv.get_path_ids(),
-        np.array([
-            [0, 0],
-            [1, 0],
-            [2, 0],
-            [3, 0],
-            [1, 1],
-            [2, 1],
-            [3, 1],
-            [2, 2],
-            [3, 2],
-            [3, 3],
-        ]),
+        np.array(
+            [
+                [0, 0],
+                [1, 0],
+                [2, 0],
+                [3, 0],
+                [1, 1],
+                [2, 1],
+                [3, 1],
+                [2, 2],
+                [3, 2],
+                [3, 3],
+            ]
+        ),
     )
+
+
+def optimal_folds_number_full_search(
+        n_observations: int,
+        target_train_size: int,
+        target_n_test_paths: int,
+) -> tuple[int, int]:
+    def _cost(
+            x: int,
+            y: int,
+    ) -> float:
+        n_test_paths = _n_test_paths(n_folds=x, n_test_folds=y)
+        avg_train_size = _avg_train_size(
+            n_observations=n_observations, n_folds=x, n_test_folds=y
+        )
+        return (
+                abs(n_test_paths - target_n_test_paths)
+                / target_n_test_paths
+                + abs(avg_train_size - target_train_size)
+                / target_train_size
+        )
+
+    res = []
+    costs = []
+    for n_folds in range(3, n_observations + 1):
+        for n_test_folds in range(2, n_folds):
+            res.append((n_folds, n_test_folds))
+            costs.append(_cost(x=n_folds, y=n_test_folds))
+    i = np.argmin(costs)
+    return res[i]
+
+
+@pytest.mark.parametrize("n_observations,target_n_test_paths,target_train_size,expected",
+                        [
+                            (10, 10, 1, (10, 9)),
+                            (10, 2, 100,  (3, 2)),
+                            (10, 2, 5,  (3, 2)),
+                            (100, 20, 10, (21, 20)),
+                            (100, 5, 30,  (6, 5)),
+                            (1000, 300, 50, (26, 24)),
+                        ]
+                        )
+def test_optimal_folds_number(
+        n_observations: int, target_train_size: int, target_n_test_paths: int,
+        expected: tuple[int, int]):
+    res = optimal_folds_number(
+        n_observations=n_observations,
+        target_train_size=target_train_size,
+        target_n_test_paths=target_n_test_paths,
+    )
+    assert res == expected
+
+
+def test_optimal_folds_number_weight():
+    n_observations = 5000
+    target_train_size = 250
+    target_n_test_paths = 50
+
+    n_folds, n_test_folds = optimal_folds_number(
+        n_observations=n_observations, target_train_size=target_train_size,
+        target_n_test_paths=target_n_test_paths,
+    )
+    avg_train_size = n_observations / n_folds * (n_folds - n_test_folds)
+    n_test_paths = int(math.comb(n_folds, n_test_folds)) * n_test_folds // n_folds
+
+    assert n_folds == 51
+    assert n_test_folds == 50
+    assert int(avg_train_size) == 98
+    assert n_test_paths == 50
+
+    n_folds, n_test_folds = optimal_folds_number(
+        n_observations=n_observations, target_train_size=target_train_size,
+        target_n_test_paths=target_n_test_paths,
+        weight_train_size=2,
+    )
+    avg_train_size = n_observations / n_folds * (n_folds - n_test_folds)
+    n_test_paths = int(math.comb(n_folds, n_test_folds)) * n_test_folds // n_folds
+
+    assert n_folds == 20
+    assert n_test_folds == 19
+    assert int(avg_train_size) == 250
+    assert n_test_paths == 19
