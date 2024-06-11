fix(covariance): ImpliedCovariance metadata routing

src/skfolio/moments/covariance/_implied_covariance.py
@@ -12,7 +12,8 @@ import numpy.typing as npt
 import sklearn as sk
 import sklearn.base as skb
 import sklearn.linear_model as skl
-import sklearn.metrics as skm
+import sklearn.metrics as sks
+import sklearn.utils.metadata_routing as skm
 
 import skfolio.typing as skt
 from skfolio.moments.covariance._base import BaseCovariance
@@ -105,6 +106,18 @@ class ImpliedCovariance(BaseCovariance):
         self.window = window
         self.volatility_risk_premium_adj = volatility_risk_premium_adj
 
+    def get_metadata_routing(self):
+        # noinspection PyTypeChecker
+        router = (
+            skm.MetadataRouter(owner=self.__class__.__name__)
+            .add_self_request(self)
+            .add(
+                covariance_estimator=self.covariance_estimator,
+                method_mapping=skm.MethodMapping().add(caller="fit", callee="fit"),
+            )
+        )
+        return router
+
     def fit(
         self, X: npt.ArrayLike, y=None, implied_vol: npt.ArrayLike = None, **fit_params
     ) -> "ImpliedCovariance":
@@ -126,13 +139,19 @@ class ImpliedCovariance(BaseCovariance):
         self : ImpliedCovariance
             Fitted estimator.
         """
+        if implied_vol is not None:
+            fit_params["implied_vol"] = implied_vol
+
+        routed_params = skm.process_routing(self, "fit", **fit_params)
+
         # fitting estimators
         self.covariance_estimator_ = check_estimator(
             self.covariance_estimator,
             default=EmpiricalCovariance(),
             check_type=BaseCovariance,
         )
-        self.covariance_estimator_.fit(X)
+        self.covariance_estimator_.fit(X, y, **routed_params.covariance_estimator.fit)
+
         covariance = self.covariance_estimator_.covariance_
 
         assets_names = get_feature_names(X)
@@ -233,7 +252,7 @@ class ImpliedCovariance(BaseCovariance):
             model.fit(X=X_train, y=y_train)
             self.coefs_[i, :] = model.coef_
             self.intercepts_[i] = model.intercept_
-            self.r2_scores_[i] = skm.r2_score(y_train, model.predict(X_train))
+            self.r2_scores_[i] = sks.r2_score(y_train, model.predict(X_train))
             rv_pred = model.predict(X_pred)
             self.pred_realised_vols_[i] = np.exp(rv_pred[0])
             self.linear_regressors_.append(model)

tests/test_moment/test_covariance/test_implied_covariance.py
@@ -3,9 +3,11 @@ import datetime as dt
 import numpy as np
 import pytest
 import sklearn.linear_model as skl
+from sklearn import config_context
+from sklearn.exceptions import UnsetMetadataPassedError
 
 from skfolio.datasets import load_sp500_dataset, load_sp500_implied_vol_dataset
-from skfolio.moments import ImpliedCovariance
+from skfolio.moments import ImpliedCovariance, LedoitWolf
 from skfolio.moments.covariance._implied_covariance import (
     _compute_implied_vol,
     _compute_realised_vol,
@@ -164,3 +166,46 @@ def test_implied_covariance_small(X, implied_vol, window):
     model = ImpliedCovariance(window=window)
     model.fit(X, implied_vol=implied_vol)
     assert np.any(model.coefs_ != 0)
+
+
+def test_implied_covariance_meta_data_routing_error(X, implied_vol):
+    with config_context(enable_metadata_routing=True):
+        model = ImpliedCovariance(
+            covariance_estimator=ImpliedCovariance()
+        )
+        with pytest.raises(UnsetMetadataPassedError):
+            model.fit(X, implied_vol=implied_vol)
+
+
+def test_implied_covariance_meta_data_routing(X, implied_vol):
+    with config_context(enable_metadata_routing=True):
+        model = ImpliedCovariance(
+            covariance_estimator=ImpliedCovariance().set_fit_request(implied_vol=True)
+        )
+        model.fit(X, implied_vol=implied_vol)
+
+        model_ref = ImpliedCovariance()
+        model_ref.fit(X, implied_vol=implied_vol)
+
+        np.testing.assert_almost_equal(model.covariance_, model_ref.covariance_)
+
+
+def test_implied_covariance_ledoit_wolf(X, implied_vol):
+    model = ImpliedCovariance(
+        covariance_estimator=LedoitWolf()
+    )
+    model.fit(X, implied_vol=implied_vol)
+
+    model_imp_ref = ImpliedCovariance()
+    model_imp_ref.fit(X, implied_vol=implied_vol)
+
+    model_led_ref = LedoitWolf()
+    model_led_ref.fit(X)
+
+    np.testing.assert_almost_equal(np.diag(model.covariance_),
+                                   np.diag(model_imp_ref.covariance_))
+
+    np.fill_diagonal(model.covariance_, 0)
+    np.fill_diagonal(model_led_ref.covariance_, 0)
+
+    np.testing.assert_almost_equal(model.covariance_, model_led_ref.covariance_)