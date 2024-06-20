fix(covariance): implied covariance corr_to_cov

examples/9_metadata_routing/plot_1_implied_volatility.py
@@ -54,16 +54,16 @@ model.r2_scores_
 
 #
 coefs={}
-for window in [10, 20, 60, 100]:
-    model = ImpliedCovariance(window=window)
+for window_size in [10, 20, 60, 100]:
+    model = ImpliedCovariance(window_size=window_size)
     model.fit(X, implied_vol=implied_vol)
-    coefs[window]=model.r2_scores_
+    coefs[window_size]=model.r2_scores_
 
 df = pd.DataFrame(coefs, index=X.columns).unstack().reset_index().rename(
-    columns={'level_0': 'window', "level_1": "asset", 0: "R2"})
-df['window'] = df['window'].astype(str)
+    columns={'level_0': 'window_size', "level_1": "asset", 0: "R2"})
+df['window_size'] = df['window_size'].astype(str)
 fig = px.bar(df, x="asset", y="R2",
-                 color="window", barmode="group")
+                 color="window_size", barmode="group")
 fig.show()
 
 {k: np.mean(v) for k, v in coefs.items()}

src/skfolio/moments/covariance/_implied_covariance.py
@@ -20,10 +20,13 @@ import skfolio.typing as skt
 from skfolio.moments.covariance._base import BaseCovariance
 from skfolio.moments.covariance._empirical_covariance import EmpiricalCovariance
 from skfolio.utils.tools import check_estimator, get_feature_names, safe_indexing
+from skfolio.utils.stats import corr_to_cov, cov_to_corr
 
 
 class ImpliedCovariance(BaseCovariance):
     """Implied Covariance estimator.
+
+
     The covariance matrix is first estimated using a Covariance estimator (for example
     `EmpiricalCovariance`) then the diagonal elements are shrunken toward the expected
     variances computed from the implied volatilities.
@@ -75,6 +78,48 @@ class ImpliedCovariance(BaseCovariance):
     feature_names_in_ : ndarray of shape (`n_features_in_`,)
         Names of assets seen during `fit`. Defined only when `returns`
         has assets names that are all strings.
+
+
+
+    Christensen, B., Hansen, C. (2002). New evidence on the implied-realized volatility relation. The
+European Journal of Finance. 8(2): 187-205.
+
+    Christensen, B., Prabhala, N. (1998). The relation between implied and realized volatility.
+Journal of Financial Economics. 50: 125-150.
+
+Egbers, T., Swinkels, L. (2015). Can implied volatility predict returns on the carry trade?.
+Journal of Banking and Finance. 59: 14-26.
+
+Andersen,T. G., Bollerslev, T., Christoffersen, P. F., & Diebold, F.X. (2006).
+ Volatility and correlation forecasting. in G. Elliott, C.W.J. Granger, and A.
+ Timmermann (eds.), Handbook of Economic Forecasting. Amsterdam: North-Holland,
+
+How Well Does Implied Volatility Predict
+Future Stock Index Returns and Volatility?
+A study of Option-Implied Volatility Derived from OMXS30 Index Options
+Authors: Sara Vikberg and Julia Björkman
+
+
+    Christensen & Prabhala (1998)
+    Christensen and Hansen (2002)
+    Egbers and Swinkels (2015)
+
+    monthly nonoverlapping data in our study to avoid
+    possible regression errors caused by autocorrelation.
+
+    use logarithmical volatility data due to its better finite sample properties
+    compared to nonlogarithmized data.
+
+    As typically the case with volatility measures (see Andersen et al. (2006) for
+instance), a simple logarithmic transformation would almost lead to normality, we notice
+from Table 1 that the distribution of log-volatility series are less skewed and leptokurtic
+compared to that of the level series, which reveals that the log volatility is more
+conformable with the normal distribution. Regressions based on the log volatility are thus
+statistically better specified than those based on level series.
+
+
+
+
     """
 
     covariance_estimator_: BaseCovariance
@@ -88,7 +133,7 @@ class ImpliedCovariance(BaseCovariance):
         self,
         covariance_estimator: BaseCovariance | None = None,
         annualized_factor: float = 252.0,
-        window: int = 21,
+        window_size: int = 21,
         linear_regressor: skb.BaseEstimator | None = None,
         volatility_risk_premium_adj: skt.MultiInput | None = None,
         nearest: bool = False,
@@ -103,7 +148,7 @@ class ImpliedCovariance(BaseCovariance):
         self.covariance_estimator = covariance_estimator
         self.annualized_factor = annualized_factor
         self.linear_regressor = linear_regressor
-        self.window = window
+        self.window_size = window_size
         self.volatility_risk_premium_adj = volatility_risk_premium_adj
 
     def get_metadata_routing(self):
@@ -161,7 +206,7 @@ class ImpliedCovariance(BaseCovariance):
         # noinspection PyArgumentList
         self.covariance_estimator_.fit(X, y, **routed_params.covariance_estimator.fit)
 
-        covariance = self.covariance_estimator_.covariance_
+        corr, _ = cov_to_corr(self.covariance_estimator_.covariance_)
 
         assets_names = get_feature_names(X)
         if assets_names is not None:
@@ -194,10 +239,10 @@ class ImpliedCovariance(BaseCovariance):
                 implied_vol[-1] / self.volatility_risk_premium_adj
             )
         else:
-            if self.window is None or self.window < 3:
+            if self.window_size is None or self.window_size < 3:
                 raise ValueError(
                     f"window must be strictly greater than 2, "
-                    f"received {self.window}"
+                    f"received {self.window_size}"
                 )
             _linear_regressor = check_estimator(
                 self.linear_regressor,
@@ -209,7 +254,7 @@ class ImpliedCovariance(BaseCovariance):
                 linear_regressor=_linear_regressor, returns=X, implied_vol=implied_vol
             )
 
-        np.fill_diagonal(covariance, self.pred_realised_vols_**2)
+        covariance = corr_to_cov(corr, self.pred_realised_vols_)
 
         self._set_covariance(covariance)
         return self
@@ -222,21 +267,21 @@ class ImpliedCovariance(BaseCovariance):
     ) -> None:
         n_observations, n_assets = returns.shape
 
-        n_folds = n_observations // self.window
+        n_folds = n_observations // self.window_size
         if n_folds < 3:
             raise ValueError(
                 f"Not enough observations to compute the volatility regression "
-                f"coefficients. The window size of {self.window} on {n_observations} "
+                f"coefficients. The window size of {self.window_size} on {n_observations} "
                 f"observations produces {n_folds} non-overlapping folds. "
                 f"The minimum number of fold is 3. You can either increase the number "
                 f"of observation in your training set or decrease the window size."
             )
 
         realised_vol = _compute_realised_vol(
-            returns=returns, window=self.window, ddof=1
+            returns=returns, window_size=self.window_size, ddof=1
         )
 
-        implied_vol = _compute_implied_vol(implied_vol=implied_vol, window=self.window)
+        implied_vol = _compute_implied_vol(implied_vol=implied_vol, window_size=self.window_size)
 
         if realised_vol.shape != implied_vol.shape:
             raise ValueError("`realised_vol`and `implied_vol` must have same shape")
@@ -268,25 +313,25 @@ class ImpliedCovariance(BaseCovariance):
 
 
 def _compute_realised_vol(
-    returns: np.ndarray, window: int, ddof: int = 1
+    returns: np.ndarray, window_size: int, ddof: int = 1
 ) -> np.ndarray:
     n_observations, n_assets = returns.shape
-    chunks = n_observations // window
+    chunks = n_observations // window_size
 
     return np.std(
         np.reshape(
-            returns[n_observations - chunks * window :, :], (chunks, window, n_assets)
+            returns[n_observations - chunks * window_size:, :], (chunks, window_size, n_assets)
         ),
         ddof=ddof,
         axis=1,
     )
 
 
-def _compute_implied_vol(implied_vol: np.ndarray, window: int) -> np.ndarray:
+def _compute_implied_vol(implied_vol: np.ndarray, window_size: int) -> np.ndarray:
     n_observations, _ = implied_vol.shape
-    chunks = n_observations // window
+    chunks = n_observations // window_size
     return implied_vol[
-        np.arange(n_observations - (chunks - 1) * window - 1, n_observations, window)
+        np.arange(n_observations - (chunks - 1) * window_size - 1, n_observations, window_size)
     ]
 
 

tests/test_moment/test_covariance/test_implied_covariance.py
@@ -14,7 +14,7 @@ from skfolio.moments.covariance._implied_covariance import (
 def test_compute_realised_vol(X):
     n_observations, n_assets = X.shape
     window = 30
-    rv = _compute_realised_vol(np.array(X), window=window, ddof=0)
+    rv = _compute_realised_vol(np.array(X), window_size=window, ddof=0)
     assert rv.shape == (n_observations // window, n_assets)
 
     res = []
@@ -23,7 +23,7 @@ def test_compute_realised_vol(X):
         res.append(
             np.asarray(
                 X.iloc[
-                    n_observations - (k + 1) * window : n_observations - k * window
+                n_observations - (k + 1) * window: n_observations - k * window
                 ].std(axis=0, ddof=0)
             )
         )
@@ -38,7 +38,7 @@ def test_compute_implied_vol(implied_vol):
     n_observations, n_assets = implied_vol.shape
     window = 30
 
-    iv = _compute_implied_vol(np.asarray(implied_vol), window=window)
+    iv = _compute_implied_vol(np.asarray(implied_vol), window_size=window)
     assert iv.shape == (n_observations // window, n_assets)
 
     res = []
@@ -69,7 +69,7 @@ def test_implied_covariance_default(X, implied_vol):
 
     res = np.exp(
         model.coefs_[:, 0] * np.log(np.asarray(implied_vol.iloc[-1] / np.sqrt(252)))
-        + model.coefs_[:, 1] * np.log(np.asarray(X[-model.window :].std(ddof=1)))
+        + model.coefs_[:, 1] * np.log(np.asarray(X[-model.window_size:].std(ddof=1)))
         + model.intercepts_
     )
 
@@ -77,58 +77,20 @@ def test_implied_covariance_default(X, implied_vol):
 
     np.testing.assert_almost_equal(
         model.covariance_[:2][:2],
-        np.array(
-            [
-                [
-                    6.57488641e-04,
-                    2.83134035e-04,
-                    1.56184082e-04,
-                    1.75814250e-04,
-                    1.22056078e-04,
-                    1.38660138e-04,
-                    1.47083022e-04,
-                    8.10806087e-05,
-                    1.42473991e-04,
-                    8.01598741e-05,
-                    9.68377637e-05,
-                    7.98446492e-05,
-                    2.15007353e-04,
-                    9.97345580e-05,
-                    8.60888400e-05,
-                    8.56609339e-05,
-                    1.37569074e-04,
-                    1.32999989e-04,
-                    8.21417385e-05,
-                    1.06253398e-04,
-                ],
-                [
-                    2.83134035e-04,
-                    1.01675318e-03,
-                    2.20576378e-04,
-                    2.51503645e-04,
-                    1.76240627e-04,
-                    1.87494904e-04,
-                    2.00154514e-04,
-                    8.31528862e-05,
-                    1.81429439e-04,
-                    8.63395567e-05,
-                    1.16628151e-04,
-                    9.03413258e-05,
-                    2.57876321e-04,
-                    1.02332010e-04,
-                    1.10774884e-04,
-                    8.74268879e-05,
-                    2.68487267e-04,
-                    1.62195796e-04,
-                    8.62525325e-05,
-                    1.38431233e-04,
-                ],
-            ]
-        ),
+        np.array([[6.57488641e-04, 3.38029358e-04, 2.20436830e-04, 2.71011063e-04,
+                   1.77192570e-04, 1.98004521e-04, 2.43218235e-04, 9.48343910e-05,
+                   2.06384336e-04, 1.08629264e-04, 1.44170060e-04, 1.06730837e-04,
+                   3.82153987e-04, 1.29396828e-04, 1.28674960e-04, 1.14263570e-04,
+                   1.77041058e-04, 1.61847771e-04, 1.09866425e-04, 1.65322496e-04],
+                  [3.38029358e-04, 1.01675318e-03, 1.90423048e-04, 2.37132104e-04,
+                   1.56496724e-04, 1.63766879e-04, 2.02447353e-04, 5.94893621e-05,
+                   1.60754088e-04, 7.15669895e-05, 1.06205460e-04, 7.38658472e-05,
+                   2.80355882e-04, 8.12087064e-05, 1.01274852e-04, 7.13317911e-05,
+                   2.11344039e-04, 1.20727952e-04, 7.05644697e-05, 1.31745666e-04]])
     )
 
     np.testing.assert_almost_equal(
-        np.diag(model.covariance_), model.pred_realised_vols_**2
+        np.diag(model.covariance_), model.pred_realised_vols_ ** 2
     )
     np.testing.assert_almost_equal(
         model.pred_realised_vols_,
@@ -169,7 +131,7 @@ def test_implied_covariance_no_intercept(X, implied_vol):
 
 @pytest.mark.parametrize("volatility_risk_premium_adj", [0.1, 1, 1.5, 100])
 def test_implied_covariance_volatility_risk_premium_adj(
-    X, implied_vol, volatility_risk_premium_adj
+        X, implied_vol, volatility_risk_premium_adj
 ):
     model = ImpliedCovariance(volatility_risk_premium_adj=volatility_risk_premium_adj)
     model.fit(X, implied_vol=implied_vol)
@@ -180,7 +142,7 @@ def test_implied_covariance_volatility_risk_premium_adj(
 
 @pytest.mark.parametrize("volatility_risk_premium_adj", [-0.5, 0])
 def test_implied_covariance_volatility_risk_premium_adj_non_pos(
-    X, implied_vol, volatility_risk_premium_adj
+        X, implied_vol, volatility_risk_premium_adj
 ):
     model = ImpliedCovariance(volatility_risk_premium_adj=volatility_risk_premium_adj)
     with pytest.raises(ValueError):
@@ -189,21 +151,21 @@ def test_implied_covariance_volatility_risk_premium_adj_non_pos(
 
 @pytest.mark.parametrize("n_folds", [0.1, 1, 2])
 def test_implied_covariance_window_too_big(X, implied_vol, n_folds):
-    model = ImpliedCovariance(window=len(X) // n_folds)
+    model = ImpliedCovariance(window_size=len(X) // n_folds)
     with pytest.raises(ValueError):
         model.fit(X, implied_vol=implied_vol)
 
 
-@pytest.mark.parametrize("window", [-1, 0, 1, 2])
-def test_implied_covariance_small_error(X, implied_vol, window):
-    model = ImpliedCovariance(window=window)
+@pytest.mark.parametrize("window_size", [-1, 0, 1, 2])
+def test_implied_covariance_small_error(X, implied_vol, window_size):
+    model = ImpliedCovariance(window_size=window_size)
     with pytest.raises(ValueError):
         model.fit(X, implied_vol=implied_vol)
 
 
-@pytest.mark.parametrize("window", [3, 4])
-def test_implied_covariance_small(X, implied_vol, window):
-    model = ImpliedCovariance(window=window)
+@pytest.mark.parametrize("window_size", [3, 4])
+def test_implied_covariance_small(X, implied_vol, window_size):
+    model = ImpliedCovariance(window_size=window_size)
     model.fit(X, implied_vol=implied_vol)
     assert np.any(model.coefs_ != 0)
 
@@ -245,4 +207,4 @@ def test_implied_covariance_ledoit_wolf(X, implied_vol):
     np.fill_diagonal(model.covariance_, 0)
     np.fill_diagonal(model_led_ref.covariance_, 0)
 
-    np.testing.assert_almost_equal(model.covariance_, model_led_ref.covariance_)
+    np.testing.assert_almost_equal(model.covariance_, model_led_ref.covariance_, 3)