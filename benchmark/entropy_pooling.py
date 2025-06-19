9e6b36b22fef77bb184c1e404986d3a06b9478ce
HugoDelatte <delatte.hugo@gmail.com>
2025-06-19 14:15:06 +0200

    chore: update entropy pooling benchmark

--- benchmark/entropy_pooling.py
+++ benchmark/entropy_pooling.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from skfolio.datasets import load_sp500_dataset
-from skfolio.measures import cvar, mean, standard_deviation
+from skfolio.measures import cvar, mean, standard_deviation, variance
 from skfolio.preprocessing import prices_to_returns
 from skfolio.prior import EntropyPooling
 from skfolio.utils.figure import plot_kde_distributions
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print(f"Ann. Vol: {standard_deviation(X['AMD']) * np.sqrt(252):0.1%}")
     print(f"CVaR-95%: {cvar(X['AMD']):0.1%}")
 
-    # Entropy Pooling with only a CVaR view to see how it affect the mean
+    # Entropy Pooling with only a CVaR view to see how it affects the mean
     ep = EntropyPooling(cvar_views=["AMD == 0.15"])
     ep.fit(X)
 
@@ -25,9 +25,9 @@ if __name__ == "__main__":
 
     # Results
     print(f"Relative Entropy : {ep.relative_entropy_:.3f}")
-    print(f"Ann. Mean: {mean(X['AMD'], sample_weight=sample_weight) * 252:0.1%}")
+    print(f"Ann. Mean: {mean(X['AMD'], sample_weight=sample_weight) * 252:0.0%}")
     print(
-        f"Ann. Vol: {standard_deviation(X['AMD'], sample_weight=sample_weight) * np.sqrt(252):0.1%}"
+        f"Ann. Vol: {standard_deviation(X['AMD'], sample_weight=sample_weight) * np.sqrt(252):0.0%}"
     )
     print(f"CVaR-95%: {cvar(X['AMD'], sample_weight=sample_weight):0.1%}")
 
@@ -38,19 +38,22 @@ if __name__ == "__main__":
         X[["AMD"]],
         sample_weight=sample_weight,
         percentile_cutoff=0.1,
-        title="Distribution of Asset Returns (Prior vs. Posterior)",
+        title="Distribution of AMD Returns (Prior vs. Posterior)",
         unweighted_suffix="Prior",
         weighted_suffix="Posterior",
     ).show()
 
     # Entropy Pooling with a CVaR view plus a variance view.
     # We compare by applying the CVaR at the first stage vs the final stage.
+
+    vol_view = 0.8
+    variance_view = vol_view**2/252
     for apply_cvar_last in [True, False]:
         print("\n")
         print(f"Apply CVaR Last: {apply_cvar_last}")
 
         ep = EntropyPooling(
-            variance_views=["AMD == 0.0025"],
+            variance_views=[f"AMD == {variance_view}"],
             cvar_views=["AMD == 0.15"],
             apply_cvar_last=apply_cvar_last,
         )
@@ -60,9 +63,9 @@ if __name__ == "__main__":
 
         # Results
         print(f"Relative Entropy : {ep.relative_entropy_:.3f}")
-        print(f"Ann. Mean: {mean(X['AMD'], sample_weight=sample_weight) * 252:0.1%}")
+        print(f"Ann. Mean: {mean(X['AMD'], sample_weight=sample_weight) * 252:0.0%}")
         print(
-            f"Ann. Vol: {standard_deviation(X['AMD'], sample_weight=sample_weight) * np.sqrt(252):0.1%}"
+            f"Ann. Vol: {standard_deviation(X['AMD'], sample_weight=sample_weight) * np.sqrt(252):0.0%}"
         )
         print(f"CVaR-95%: {cvar(X['AMD'], sample_weight=sample_weight):0.1%}")
 
@@ -71,7 +74,7 @@ if __name__ == "__main__":
             X[["AMD"]],
             sample_weight=ep.return_distribution_.sample_weight,
             percentile_cutoff=0.1,
-            title="Distribution of Asset Returns (Prior vs. Posterior)",
+            title="Distribution of AMD Returns (Prior vs. Posterior)",
             unweighted_suffix="Prior",
             weighted_suffix="Posterior",
         ).show()
