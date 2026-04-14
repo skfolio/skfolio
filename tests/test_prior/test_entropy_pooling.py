bbea0143d5ac724598c215546529319d987641f3
HugoDelatte <delatte.hugo@gmail.com>
2026-04-14 09:52:23 +0200

    test: unit tests

--- tests/test_prior/test_entropy_pooling.py
+++ tests/test_prior/test_entropy_pooling.py
@@ -630,9 +630,9 @@ def test_skew_views(X, solver):
     np.testing.assert_almost_equal(model.relative_entropy_, 0.010719, 5)
     assert np.all(sw >= 0)
     np.testing.assert_almost_equal(np.sum(sw), 1)
-    np.testing.assert_almost_equal(skew[0], -0.1, 5)
+    np.testing.assert_almost_equal(skew[0], -0.1, 4)
     np.testing.assert_almost_equal(skew[2], 2.0, 4)
-    np.testing.assert_almost_equal(skew[18], -0.3, 5)
+    np.testing.assert_almost_equal(skew[18], -0.3, 4)
     np.testing.assert_almost_equal(2.5 * skew[3] - (1.4 * skew[4] + 4 * skew[5]), 0, 4)
 
 
